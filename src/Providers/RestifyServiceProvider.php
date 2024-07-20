0e166eb5cee495f2cc4b23c4aec4935366c671bc
Mikhail Protasevich <m.protasevich@smartcat.ai>
2024-07-20 17:01:53 +0300

    [WIP] Initialized a basic package structure

---
+++ src/Providers/RestifyServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Somecode\Restify\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RestifyServiceProvider extends ServiceProvider
+{
+    public function boot() {}
+
+    public function register() {}
+}
